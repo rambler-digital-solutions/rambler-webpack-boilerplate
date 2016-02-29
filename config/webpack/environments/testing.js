d8ffc1a5f389ef262148ad5c0947b9bd4f7701ab
Chernobrov Mike <m.chernobrov@rambler-co.ru>
2016-02-29 16:23:02 +0300

    add karma webpack intergation

--- config/webpack/environments/testing.js
+++ config/webpack/environments/testing.js
@@ -1,20 +1,87 @@
 'use strict';
 
-/**
- * Development config
- * @param  {String} _path Absolute path to application
- * @return {Object}       Object of development settings
- */
+// Depends
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = function(_path) {
+  var rootAssetPath = './app/assets';
   return {
-    context: _path,
-    debug: true,
-    devtool: 'eval',
-    devServer: {
-      contentBase: './dist',
-      info: true,
-      hot: false,
-      inline: true
-    }
+    cache: true,
+    devtool: 'inline-source-map',
+    // resolves modules
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+      modulesDirectories: ['node_modules'],
+      alias: {
+        _svg:         path.join(_path, 'app', 'assets', 'svg'),
+        _data:        path.join(_path, 'app', 'data'),
+        _fonts:       path.join(_path, 'app', 'assets', 'fonts'),
+        _modules:     path.join(_path, 'app', 'modules'),
+        _images:      path.join(_path, 'app', 'assets', 'images'),
+        _stylesheets: path.join(_path, 'app', 'assets', 'stylesheets'),
+        _templates:   path.join(_path, 'app', 'assets', 'templates')
+      }
+    },
+    module: {
+      preLoaders: [
+        {
+          test: /.spec\.js$/,
+          include: /src/,
+          exclude: /(bower_components|node_modules)/,
+          loader: 'babel',
+          query: {
+            presets: ['es2015'],
+            cacheDirectory: true,
+          }
+        },
+        {
+          test: /\.js?$/,
+          include: /app\/assets\/javascripts\/core/,
+          exclude: /(node_modules|__tests__)/,
+          loader: 'babel-istanbul',
+          query: {
+            cacheDirectory: true,
+          },
+        },
+      ],
+      loaders: [
+        // es6 loader
+        {
+          include: path.join(_path, 'app', 'assets', 'javascripts', 'core'),
+          loader: 'babel-loader',
+          exclude: /(node_modules|__tests__)/,
+          query: {
+            presets: ['es2015'],
+            cacheDirectory: true,
+          }
+        },
+
+        // jade templates
+        {
+          test: /\.jade$/,
+          loader: 'jade-loader'
+        },
+
+        // stylus loader
+        {
+          test: /\.styl$/,
+          loader: 'style!css!stylus'
+        },
+
+        // external files loader
+        {
+          test: /\.(png|ico|jpg|jpeg|gif|svg|ttf|eot|woff|woff2)$/i,
+          loader: 'file',
+          query: {
+            context: rootAssetPath,
+            name: '[path][hash].[name].[ext]'
+          }
+        }
+      ],
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ]
   };
 };
